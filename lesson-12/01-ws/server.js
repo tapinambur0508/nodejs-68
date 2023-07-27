1cc248dfd38353843ad4004c49c1ab5f13b8b13d
Vitalii Mudryi <vmudrij0508@gmail.com>
2023-07-27 23:10:18 +0300

    lesson 12

---
+++ lesson-12/01-ws/server.js
@@ -0,0 +1,31 @@
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const clients = [];
+
+wss.on("connection", (socket) => {
+  clients.push(socket);
+
+  for (const client of clients) {
+    if (client === socket) {
+      client.send("Welcome to Chat");
+    } else {
+      client.send("New user connected to Chat");
+    }
+  }
+
+  socket.on("message", (message) => {
+    const data = JSON.parse(message.toString("utf-8"));
+
+    for (const client of clients) {
+      if (client === socket) {
+        client.send(`You: ${data.message}`);
+      } else {
+        client.send(`${data.name}: ${data.message}`);
+      }
+    }
+  });
+});
+
+console.log("Server is running on port 8080");
---
+++ lesson-12/03-scoket.io-express/server.js
@@ -0,0 +1,31 @@
+const http = require("node:http");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (__, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/ping", (__, res) => {
+  return res.send("pong");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("chatMessage", "Welcome to Chat");
+  socket.broadcast.emit("chatMessage", "New user connected to Chat");
+
+  socket.on("chatMessage", (message) => {
+    const data = JSON.parse(message);
+
+    socket.emit("chatMessage", `You: ${data.message}`);
+    socket.broadcast.emit("chatMessage", `${data.name}: ${data.message}`);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
